57e513e1a15eeb2dc08ddd437c4e336f0dad695e
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-05-06 16:22:07 -0500

    end of lecture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import Animals from './Components/Animals/Animals.js'
 import Plants from './Components/Plants/Plants.js'
@@ -8,9 +9,22 @@ import Home from './Components/Home/Home.js'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
+      <div>
+        <Router>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/plants'>Plants</Link></li>
+            <li><Link to='/animals'>Animals</Link></li>
+          </ul>
+          <div className="App">
+            <h1>Baconian React Router</h1>
+            <Route exact path='/' component={ Home }/>
+            <Route path='/plants' component={ Plants }/>
+            <Route path='/animals' component={ Animals }/>
+          </div>
+        </Router>
       </div>
+      
     );
   }
 }
